6ea1ee4c173c464f22df1b86daeac535e4b6a271
Da1sukeKud0 <yokohamadiceke@yahoo.co.jp>
2020-07-21 17:32:21 +0900

    Initial commit

---
+++ lib/getFileNameList.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+import pyperclip as pc
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        print('Error.')
+    else:
+        full_path = str(sys.argv[1])
+        filename_list = []
+
+        for p, ds, fs in os.walk(full_path):
+            for f in fs:
+                filename_list.append(f)
+
+        # ファイル名毎に改行して取得
+        pc.copy('\r\n'.join(filename_list))
